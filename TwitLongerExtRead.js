847073b274546e768080549f41b4c243496b5083
Collin Bartlam <collinbartlam@gmail.com>
2021-10-02 19:14:51 -0400

    update to css class name

--- TwitLongerExtRead.js
+++ TwitLongerExtRead.js
@@ -1,7 +1,7 @@
 checkTweetOverflow();
 
 function checkTweetOverflow() {
-    tweetOverflowMarker = document.getElementsByClassName("css-901oao r-daml9f r-n6v787 r-19jlu03 r-285fr0 r-q4m81j");
+    tweetOverflowMarker = document.getElementsByClassName("css-901oao r-9l7dzd r-n6v787 r-19jlu03 r-285fr0 r-q4m81j");
     if (tweetOverflowMarker.length > 0) {
         tweetOverflowTextArr = document.getElementsByClassName("public-DraftStyleDefault-block public-DraftStyleDefault-ltr");
         tweetOverflowText = tweetOverflowTextArr[0].innerText;
